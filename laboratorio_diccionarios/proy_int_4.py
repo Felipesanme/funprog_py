98af85eb3fe861198e39b9b76fc7f876d4ccc823
Felipe Sánchez Mejía <felipe@Felipes-MacBook-Pro.local>
2022-08-07 04:56:27 -0500

    implementación de funciones cumpliendo con el enunciado

--- laboratorio_diccionarios/proy_int_4.py
+++ laboratorio_diccionarios/proy_int_4.py
@@ -1,7 +1,7 @@
 import readchar
 import os
-os.system('cls' if os.name == 'nt' else 'clear')
-maze = '''..#######################
+
+maze='''p.#######################
 ......#.....#.#.....#...#
 #.#######.###.#####.###.#
 #...............#.......#
@@ -27,61 +27,69 @@ maze = '''..#######################
 #.......#.......#.....#.#
 #######################.#'''
 
-print(maze)
+mat_maze=[]
 
-mat_maze = list(maze.split("\n"))
+def clean_screen_show(maze:str)->list:
 
-# print(mat_maze)
-# print(len(mat_maze))
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-for i in range(len(mat_maze)):
-    mat_maze[i] = list(mat_maze[i])
+    print(maze)
+
+    mat_maze = list(maze.split("\n"))
+
+    for i in range(len(mat_maze)):
+        mat_maze[i] = list(mat_maze[i])
+    return(mat_maze)
 
 px = 0
 py = 0
-player_pos = [px, py]
 start = (px,py)
-end = (len(mat_maze),len(mat_maze))
+end = (len(clean_screen_show(maze)),len(clean_screen_show(maze)))
 
-while start[0] <= py <= end[0] and start[1] <= px <= end[1]:
-    tecla = readchar.readkey()
+def main_loop (px,py,start,end,mat_maze):
 
-    dic_flechas = {readchar.key.UP: "Arriba", readchar.key.DOWN: "Abajo",
-                   readchar.key.LEFT: "Izquierda", readchar.key.RIGHT: "Derecha"}
+    while start[0] <= py <= end[0] and start[1] <= px <= end[1]:
+        tecla = readchar.readkey()
 
-    if dic_flechas.get(tecla) == "Arriba" and mat_maze[py-1][px] != "#":
-        py -= 1
-        mat_maze[py+1][px] = "."
-    elif dic_flechas.get(tecla) == "Abajo" and mat_maze[py + 1][px] != "#":
-        py += 1
-        mat_maze[py-1][px] = "."
-    elif dic_flechas.get(tecla) == "Izquierda" and mat_maze[py][px - 1] != "#":
-        px -= 1
-        mat_maze[py][px+1] = "."
-    elif dic_flechas.get(tecla) == "Derecha" and mat_maze[py][px + 1] != "#":
-        px += 1
-        mat_maze[py][px-1] = "."
-    else:
+        dic_flechas = {readchar.key.UP: "Arriba", readchar.key.DOWN: "Abajo",
+                       readchar.key.LEFT: "Izquierda", readchar.key.RIGHT: "Derecha"}
+
+        if py == (end[0] - 1) and px == (end[1] - 2):
+            break
+
+        if dic_flechas.get(tecla) == "Arriba" and mat_maze[py-1][px] != "#":
+            py -= 1
+            mat_maze[py+1][px] = "."
+        elif dic_flechas.get(tecla) == "Abajo" and mat_maze[py + 1][px] != "#":
+            py += 1
+            mat_maze[py-1][px] = "."
+        elif dic_flechas.get(tecla) == "Izquierda" and mat_maze[py][px - 1] != "#":
+            px -= 1
+            mat_maze[py][px+1] = "."
+        elif dic_flechas.get(tecla) == "Derecha" and mat_maze[py][px + 1] != "#":
+            px += 1
+            mat_maze[py][px-1] = "."
+        else:
+            mat_maze [py][px] = "p"
+
         mat_maze [py][px] = "p"
 
-    mat_maze [py][px] = "p"
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(py,px)
-    # print(mat_maze)
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-    maze=""
-    for row in range(len(mat_maze)):
-        maze += "".join(mat_maze[row]) + "\n"
-    print(maze)
+        # print(py,px)
 
+        maze=""
+        for row in range(len(mat_maze)):
+            maze += "".join(mat_maze[row]) + "\n"
+        print(maze)
 
-    # maze = sep.join(mat_maze)
+        if py == end[0] and px == end[1] - 1:
+            break
 
+def main():
 
-#
-# def main():
-#
-#     clean_screen(0)
-#
-# if __name__ == "__main__":
-#     main()
+    clean_screen_show(maze)
+    main_loop(px,py,start,end,clean_screen_show(maze))
+
+if __name__ == "__main__":
+    main()
